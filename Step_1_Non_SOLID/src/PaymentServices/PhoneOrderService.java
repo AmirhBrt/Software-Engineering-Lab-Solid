85d43de83475dbcc1a64d485ad16c73319cc1d45
Amirhossein Barati <amirhsn.barati@gmail.com>
2024-07-16 13:33:18 +0330

    feat: Add phase 1

--- Step_1_Non_SOLID/src/PaymentServices/PhoneOrderService.java
+++ Step_1_Non_SOLID/src/PaymentServices/PhoneOrderService.java
@@ -1,34 +1,33 @@
 package PaymentServices;
 
-public class OnlineOrderService implements OrderService {
+public class PhoneOrderService implements OrderService {
     @Override
     public void onSiteOrderRegister(String customerName) {
-        //Empty Body!
+        // Empty body!
     }
 
     @Override
     public void onlineOrderRegister(String customerName) {
-        System.out.println("online order registered for " + customerName);
+        // Empty body!
     }
 
     @Override
     public void phoneOrderRegister(String customerName) {
-
+        System.out.println("phone order registered for " + customerName);
     }
 
     @Override
     public void onSiteOrderPayment(int foodPrice) {
-        //Empty Body
+        // Empty body!
     }
 
     @Override
     public void onlineOrderPayment(int foodPrice) {
-        System.out.println("online Payment with Price : " + foodPrice + " Tomans!");
+        // Empty body!
     }
 
     @Override
     public void phoneOrderPayment(int foodPrice) {
-
+        System.out.println("phone Payment with Price : " + foodPrice + " Tomans!");
     }
-
 }
